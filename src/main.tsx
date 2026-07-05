2e1b31ed0dc20b039de94f84c629f81e037c195b
Rajeshkanna <101904336+MDsero@users.noreply.github.com>
2026-07-05 19:47:51 +0500

    Updated code

---
+++ src/main.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { Header } from "./components/portfolio/Header";
+import { Hero } from "./components/portfolio/Hero";
+import { About } from "./components/portfolio/About";
+import { Services } from "./components/portfolio/Services";
+import { Showcase } from "./components/portfolio/Showcase";
+import { Contact } from "./components/portfolio/Contact";
+import { Footer } from "./components/portfolio/Footer";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-background text-foreground">
+      <Header />
+      <main>
+        <Hero />
+        <About />
+        <Services />
+        <Showcase />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
